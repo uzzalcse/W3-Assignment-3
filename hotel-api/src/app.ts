548a2f4c91ec8f504e6f1da421d4a72a2f0e5234
Uzzal Mia <96492202+uzzalcse@users.noreply.github.com>
2024-11-19 22:58:22 +0600

    changes the port from 3000 to 3001 app.ts

--- hotel-api/src/app.ts
+++ hotel-api/src/app.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import hotelRoutes from './routes/hotelRoutes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
